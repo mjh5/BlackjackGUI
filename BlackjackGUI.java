a7e3664c23b08b6a4bae27553f5c4ee26dff6eea
mjh5 <mjh5@illinois.edu>
2018-08-15 14:39:01 -0500

    Added documentation, building game layout

--- BlackjackGUI.java
+++ BlackjackGUI.java
@@ -1,7 +1,10 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 
@@ -10,17 +13,40 @@ public class BlackjackGUI extends Application {
     //Objects that make up the GUI
     Stage window;
     Scene scene;
-    Button test_button;
+    ImageView dealerCard1;
+    ImageView dealerCard2;
+    ImageView playerCard1;
+    ImageView playerCard2;
+    TextArea gameText;
+    Button hitButton;
+    Button standButton;
 
 
+    /**
+     * The start method is an abstract method inherited from the Application class. This is essentially your main method
+     * for the application. Inside you build your window out of different GUI nodes, such as buttons, menus, text,
+     * images, and much more.
+     *
+     * @param primaryStage - Stage passed in when the application is run
+     */
     @Override
     public void start(Stage primaryStage) {
+        //Sets the window to the primaryStage and sets the title
         window = primaryStage;
         window.setTitle("Blackjack");
-        test_button = new Button("Test Button");
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(test_button);
+        HBox dealerHand = new HBox();
+        dealerHand.getChildren().addAll(dealerCard1, dealerCard2);
+        HBox playerHand = new HBox();
+        dealerHand.getChildren().addAll(playerCard1, playerCard2);
+        HBox hitOrStandBox = new HBox();
+        hitOrStandBox.getChildren().addAll(hitButton, standButton);
+
+        BorderPane layout = new BorderPane();
+        layout.setLeft(dealerHand);
+        layout.setRight(playerHand);
+        layout.setBottom(hitOrStandBox);
+        layout.setCenter(gameText);
 
         scene = new Scene(layout, 300, 200);
 
@@ -29,6 +55,7 @@ public class BlackjackGUI extends Application {
     }
 
     public static void main(String[] args) {
+        //To run a JavaFX application, you call the launch method and pass in args
         launch(args);
     }
 }
