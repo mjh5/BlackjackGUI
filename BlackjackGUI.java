265becb03c8f8c4ef630f461f0ecefab2133eb15
mjh5 <mjh5@illinois.edu>
2018-08-15 11:10:04 -0500

    Initiated BlackjackGUI as JavaFX Application

---
+++ BlackjackGUI.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class BlackjackGUI extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
