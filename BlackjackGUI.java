2d5959b437de67c0f217104369b0cf6059ca14dd
mjh5 <mjh5@illinois.edu>
2018-08-15 11:27:57 -0500

    Test window and button works

--- BlackjackGUI.java
+++ BlackjackGUI.java
@@ -6,9 +6,29 @@ import javafx.stage.Stage;
 
 
 public class BlackjackGUI extends Application {
-    
+
+    //Objects that make up the GUI
+    Stage window;
+    Scene scene;
+    Button test_button;
+
+
     @Override
     public void start(Stage primaryStage) {
+        window = primaryStage;
+        window.setTitle("Blackjack");
+        test_button = new Button("Test Button");
 
+        StackPane layout = new StackPane();
+        layout.getChildren().add(test_button);
+
+        scene = new Scene(layout, 300, 200);
+
+        window.setScene(scene);
+        window.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
